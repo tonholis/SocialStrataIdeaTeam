531ccc6b56f53d35bdd34d4adfc84a0f8f1c70d8
Cleyton Tonholi <tonholis@gmail.com>
2016-05-21 14:27:37 -0300

    firebase config and data prototype

---
+++ SocialStrata/app/app.services.js
@@ -0,0 +1,20 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+		.factory('FirebaseService', function() {
+			var config = {
+				apiKey: "AIzaSyB5q81AGGox4i8-QL2KOtnDDfi05irgcHE",
+				authDomain: "socialstrataideateam.firebaseapp.com",
+				databaseURL: "https://socialstrataideateam.firebaseio.com",
+				storageBucket: "",
+			};
+			return firebase.initializeApp(config);
+		});
+
+})();
+
+
+
+
