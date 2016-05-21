7f5343b40df4d9aae8c0073c30bcad323ddc31fc
Cleyton Tonholi <tonholis@gmail.com>
2016-05-21 16:29:31 -0300

    Listing buildings' channels from firebase

--- SocialStrata/app/modules/buildings/buildingsController.js
+++ SocialStrata/app/modules/buildings/buildingsController.js
@@ -11,24 +11,16 @@
 
 		$ionicLoading.show();
 		ref.on("value", function(snapshot) {
+			console.log(snapshot.val());
 			$scope.buildings = snapshot.val();
 			$ionicLoading.hide();
 		}, function(errorObject) {
 			console.log("error reading: " + errorObject.code);
 			var alertPopup = $ionicPopup.alert({
 				title: 'Ops!',
-				template: 'Sorry! An error ocurred'
+				template: 'Sorry! An error ocurred.'
 			});
 			$ionicLoading.hide();
 		});
-
-        // $scope.buildings = [
-        //     { name: 'Building 1', id: 1 },
-        //     { name: 'Building 2', id: 2 },
-        //     { name: 'Building 3', id: 3 },
-        //     { name: 'Building 4', id: 4 },
-        //     { name: 'Building 5', id: 5 },
-        //     { name: 'Building 6', id: 6 }
-        // ];
     }
 })();
