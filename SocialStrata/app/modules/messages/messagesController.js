3a1aabfb56120d72dc63c1cf2c814919f56e97a2
Cleyton Tonholi <tonholis@gmail.com>
2016-05-22 18:02:58 -0300

    channel chat fix

--- SocialStrata/app/modules/messages/messagesController.js
+++ SocialStrata/app/modules/messages/messagesController.js
@@ -8,7 +8,7 @@
             '$state',
             '$stateParams',
             '$ionicScrollDelegate',
-			'$timeout',
+            '$timeout',
             'channelsService',
             'globalsService',
             MessagesController
@@ -22,7 +22,7 @@
         this.$state = $state;
         this.$stateParams = $stateParams;
         this.$ionicScrollDelegate = $ionicScrollDelegate;
-		this.$timeout = $timeout;
+        this.$timeout = $timeout;
         this.channelsService = channelsService;
         this.globalsService = globalsService;
 
@@ -40,13 +40,13 @@
             name: globalsService.user.displayName ? globalsService.user.displayName : 'Undefined'
         };
 
-		$scope.channelKey = this.channelKey; //to use in sendMessage
+        $scope.channelKey = this.channelKey; //to use in sendMessage
         $scope.toUser;
         $scope.messages = [];
-		$scope.inputMessage = '';
-		$scope.sendMessage = function(msg) {
-			self.doSendMessage(self, msg);
-		};
+        $scope.inputMessage = '';
+        $scope.sendMessage = function(msg) {
+            self.doSendMessage(self, msg);
+        };
 
         //UI elements
         this.viewScroll = $ionicScrollDelegate.$getByHandle('userMessageScroll');
@@ -106,12 +106,12 @@
 
         firebase.database().ref(contactPath).once('value', function(snapshot) {
             var contact = snapshot.val();
-            self.$scope.toUser = {
-                userId: user.uid,
+            self.$scope.toUser = self.toUser = {
+                userId: uid,
                 userPic: 'http://ionicframework.com/img/docs/venkman.jpg',
                 userName: contact && contact.displayName ? contact.displayName : 'Undefined'
             };
-
+			
             self.getLastMessages();
         });
     };
@@ -120,16 +120,15 @@
         var self = this;
         var msgPath = ['buildings', self.buildingKey, 'messages'].join('/');
 
-        self.messageRef = firebase.database()
-            .ref(msgPath)
-            .orderByChild('channel').equalTo(self.channelKey)
+        self.messageRef = firebase.database().ref(msgPath);
+        self.messageRef.orderByChild('channel').equalTo(self.channelKey)
             .limitToLast(100)
             .on('value', function(s) {
                 self.$scope.messages = s.val();
-				
-				self.$timeout(function() {
-					self.viewScroll.scrollBottom(true);
-				}, 10);
+
+                self.$timeout(function() {
+                    self.viewScroll.scrollBottom(true);
+                }, 10);
             });
     };
 
@@ -142,14 +141,14 @@
             userId: self.$scope.user.id,
             userPic: self.$scope.user.pic
         };
-		
-		if (self.toUser)
-			message.to = self.toUser.uid; 
 
-		var msgPath = ['buildings', self.buildingKey, 'messages'].join('/');
+        if (self.toUser)
+            message.to = self.toUser.userId;
+
+        var msgPath = ['buildings', self.buildingKey, 'messages'].join('/');
         firebase.database().ref(msgPath).push(message);
-		
-		self.$scope.inputMessage = '';
+
+        self.$scope.inputMessage = '';
 
         self.$timeout(function() {
             self.keepKeyboardOpen();
@@ -159,7 +158,7 @@
 
     MessagesController.prototype.keepKeyboardOpen = function() {
         var self = this;
-		self.txtInput.one('blur', function() {
+        self.txtInput.one('blur', function() {
             console.log('textarea blur, focus back on it');
             self.txtInput[0].focus();
         });
@@ -168,279 +167,6 @@
     MessagesController.prototype.onProfilePicError = function(ele) {
         this.ele.src = ''; //fallback
     };
-
-
-
-
-    /*
-    angular
-            .module('app.messages')
-    
-            .controller('messagesController', ['$scope', '$rootScope', '$state',
-                '$stateParams', '$ionicActionSheet',
-                '$ionicPopup', '$ionicScrollDelegate', '$timeout', '$interval',
-                'channelsService', 'authService',
-                messagesController
-            ]);
-        	
-        function messagesController($scope, $rootScope, $state, $stateParams, $ionicActionSheet,
-            $ionicPopup, $ionicScrollDelegate, $timeout, $interval, channelsService, authService) {
-    
-            $scope.channelId = $state.params.channelId;
-            $scope.channelName = channelsService.channels[$scope.channelId];
-            $scope.user = authService.user();
-    
-            if (!$scope.user) {
-                $state.go('login');
-                return;
-            }
-    
-            $scope.user = {
-                _id: $scope.user.uid,
-                pic: 'http://ionicframework.com/img/docs/mcfly.jpg',
-                username: $scope.user && $scope.user.displayName ? $scope.user.displayName ? 'Anonymous';
-            };
-    
-            // mock acquiring data via $stateParams
-            $scope.toUser = null;
-            if ($scope.channelId == "landlord") {
-                $
-                $scope.toUser = {
-                    _id: '534b8e5aaa5e7afc1b23e69b',
-                    pic: 'http://ionicframework.com/img/docs/venkman.jpg',
-                    channel: $scope.channelId
-                }
-            }
-    
-            $scope.input = {
-                message: localStorage['userMessage-' + $scope.toUser._id] || ''
-            };
-    
-            var messageCheckTimer;
-    
-            var viewScroll = $ionicScrollDelegate.$getByHandle('userMessageScroll');
-            var footerBar; // gets set in $ionicView.enter
-            var scroller;
-            var txtInput; // ^^^
-    
-            $scope.$on('$ionicView.enter', function() {
-                console.log('UserMessages $ionicView.enter');
-    
-                getMessages();
-    
-                $timeout(function() {
-                    footerBar = document.body.querySelector('#userMessagesView .bar-footer');
-                    scroller = document.body.querySelector('#userMessagesView .scroll-content');
-                    txtInput = angular.element(footerBar.querySelector('textarea'));
-                }, 0);
-    
-                messageCheckTimer = $interval(function() {
-                    // here you could check for new messages if your app doesn't use push notifications or user disabled them
-                }, 20000);
-            });
-    
-            $scope.$on('$ionicView.leave', function() {
-                console.log('leaving UserMessages view, destroying interval');
-                // Make sure that the interval is destroyed
-                if (angular.isDefined(messageCheckTimer)) {
-                    $interval.cancel(messageCheckTimer);
-                    messageCheckTimer = undefined;
-                }
-            });
-    
-            $scope.$on('$ionicView.beforeLeave', function() {
-                if (!$scope.input.message || $scope.input.message === '') {
-                    localStorage.removeItem('userMessage-' + $scope.toUser._id);
-                }
-            });
-    
-            function getMessages() {
-                $scope.messages = [{
-                    "_id": "535d625f898df4e80e2a125e",
-                    "text": "Ionic has changed the game for hybrid app development.",
-                    "userId": "534b8fb2aa5e7afc1b23e69c",
-                    "date": "2014-04-27T20:02:39.082Z",
-                    "read": true,
-                    "readDate": "2014-12-01T06:27:37.944Z"
-                }, {
-                        "_id": "535f13ffee3b2a68112b9fc0",
-                        "text": "I like Ionic better than ice cream!",
-                        "userId": "534b8e5aaa5e7afc1b23e69b",
-                        "date": "2014-04-29T02:52:47.706Z",
-                        "read": true,
-                        "readDate": "2014-12-01T06:27:37.944Z"
-                    }, {
-                        "_id": "546a5843fd4c5d581efa263a",
-                        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                        "userId": "534b8fb2aa5e7afc1b23e69c",
-                        "date": "2014-11-17T20:19:15.289Z",
-                        "read": true,
-                        "readDate": "2014-12-01T06:27:38.328Z"
-                    }, {
-                        "_id": "54764399ab43d1d4113abfd1",
-                        "text": "Am I dreaming?",
-                        "userId": "534b8e5aaa5e7afc1b23e69b",
-                        "date": "2014-11-26T21:18:17.591Z",
-                        "read": true,
-                        "readDate": "2014-12-01T06:27:38.337Z"
-                    }, {
-                        "_id": "547643aeab43d1d4113abfd2",
-                        "text": "Is this magic?",
-                        "userId": "534b8fb2aa5e7afc1b23e69c",
-                        "date": "2014-11-26T21:18:38.549Z",
-                        "read": true,
-                        "readDate": "2014-12-01T06:27:38.338Z"
-                    }, {
-                        "_id": "547815dbab43d1d4113abfef",
-                        "text": "Gee wiz, this is something special.",
-                        "userId": "534b8e5aaa5e7afc1b23e69b",
-                        "date": "2014-11-28T06:27:40.001Z",
-                        "read": true,
-                        "readDate": "2014-12-01T06:27:38.338Z"
-                    }, {
-                        "_id": "54781c69ab43d1d4113abff0",
-                        "text": "I think I like Ionic more than I like ice cream!",
-                        "userId": "534b8fb2aa5e7afc1b23e69c",
-                        "date": "2014-11-28T06:55:37.350Z",
-                        "read": true,
-                        "readDate": "2014-12-01T06:27:38.338Z"
-                    }, {
-                        "_id": "54781ca4ab43d1d4113abff1",
-                        "text": "Yea, it's pretty sweet",
-                        "userId": "534b8e5aaa5e7afc1b23e69b",
-                        "date": "2014-11-28T06:56:36.472Z",
-                        "read": true,
-                        "readDate": "2014-12-01T06:27:38.338Z"
-                    }, {
-                        "_id": "5478df86ab43d1d4113abff4",
-                        "text": "Wow, this is really something huh?",
-                        "userId": "534b8fb2aa5e7afc1b23e69c",
-                        "date": "2014-11-28T20:48:06.572Z",
-                        "read": true,
-                        "readDate": "2014-12-01T06:27:38.339Z"
-                    }, {
-                        "_id": "54781ca4ab43d1d4113abff1",
-                        "text": "Create amazing apps - ionicframework.com",
-                        "userId": "534b8e5aaa5e7afc1b23e69b",
-                        "date": "2014-11-29T06:56:36.472Z",
-                        "read": true,
-                        "readDate": "2014-12-01T06:27:38.338Z"
-                    }];
-            }
-    
-            $scope.$watch('input.message', function(newValue, oldValue) {
-                console.log('input.message $watch, newValue ' + newValue);
-                if (!newValue) newValue = '';
-                localStorage['userMessage-' + $scope.toUser._id] = newValue;
-            });
-    
-            $scope.sendMessage = function(sendMessageForm) {
-                var message = {
-                    toId: $scope.toUser._id,
-                    text: $scope.input.message
-                };
-    
-                // if you do a web service call this will be needed as well as before the viewScroll calls
-                // you can't see the effect of this in the browser it needs to be used on a real device
-                // for some reason the one time blur event is not firing in the browser but does on devices
-                keepKeyboardOpen();
-    
-                //MockService.sendMessage(message).then(function(data) {
-                $scope.input.message = '';
-    
-                message._id = new Date().getTime(); // :~)
-                message.date = new Date();
-                message.username = $scope.user.username;
-                message.userId = $scope.user._id;
-                message.pic = $scope.user.picture;
-    
-                $scope.messages.push(message);
-    
-                $timeout(function() {
-                    keepKeyboardOpen();
-                    viewScroll.scrollBottom(true);
-                }, 0);
-    
-                $timeout(function() {
-                    $scope.messages.push(MockService.getMockMessage());
-                    keepKeyboardOpen();
-                    viewScroll.scrollBottom(true);
-                }, 2000);
-    
-                //});
-            };
-    
-            // this keeps the keyboard open on a device only after sending a message, it is non obtrusive
-            function keepKeyboardOpen() {
-                console.log('keepKeyboardOpen');
-                txtInput.one('blur', function() {
-                    console.log('textarea blur, focus back on it');
-                    txtInput[0].focus();
-                });
-            }
-    
-            function onProfilePicError(ele) {
-                this.ele.src = ''; // set a fallback
-            }
-    
-            $scope.onMessageHold = function(e, itemIndex, message) {
-                console.log('onMessageHold');
-                console.log('message: ' + JSON.stringify(message, null, 2));
-                $ionicActionSheet.show({
-                    buttons: [{
-                        text: 'Copy Text'
-                    }, {
-                            text: 'Delete Message'
-                        }],
-                    buttonClicked: function(index) {
-                        switch (index) {
-                            case 0: // Copy Text
-                                //cordova.plugins.clipboard.copy(message.text);
-    
-                                break;
-                            case 1: // Delete
-                                // no server side secrets here :~)
-                                $scope.messages.splice(itemIndex, 1);
-                                $timeout(function() {
-                                    viewScroll.resize();
-                                }, 0);
-    
-                                break;
-                        }
-    
-                        return true;
-                    }
-                });
-            };
-    
-            // this prob seems weird here but I have reasons for this in my app, secret!
-            $scope.viewProfile = function(msg) {
-                if (msg.userId === $scope.user._id) {
-                    // go to your profile
-                } else {
-                    // go to other users profile
-                }
-            };
-    
-            // I emit this event from the monospaced.elastic directive, read line 480
-            $scope.$on('taResize', function(e, ta) {
-                console.log('taResize');
-                if (!ta) return;
-    
-                var taHeight = ta[0].offsetHeight;
-                console.log('taHeight: ' + taHeight);
-    
-                if (!footerBar) return;
-    
-                var newFooterHeight = taHeight + 10;
-                newFooterHeight = (newFooterHeight > 44) ? newFooterHeight : 44;
-    
-                footerBar.style.height = newFooterHeight + 'px';
-                scroller.style.bottom = newFooterHeight + 'px';
-            });
-    
-        }
-        */
 })();
 
 
